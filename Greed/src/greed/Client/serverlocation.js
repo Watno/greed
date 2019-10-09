8c5d97945b5792ddf20905ccae56d4f60ebaf06e
Watno <watno@arcor.de>
2019-10-09 20:17:24 +0200

    Cleanup of project

--- Greed/src/greed/Client/serverlocation.js
+++ Greed/src/greed/Client/serverlocation.js
@@ -1,9 +1,8 @@
 /**
- * The server location is an extra file so I don't actually make the online Client point to my  local server after making changes.
+ * The server location is an extra file so I don't actually make the online Client point to my local server after making changes.
  */
 
-function connectToServer(){
-    //socket = new WebSocket('ws://ec2-35-157-160-241.eu-central-1.compute.amazonaws.com:8080/greed');
-    socket = new WebSocket('ws://192.168.1.114:8080/greed');
+function connectToServer() {
+    socket = new WebSocket('ws://192.168.178.21:8080/greed');
     return socket;
 }
--- Greed/src/greed/meta/Main.java
+++ Greed/src/greed/meta/Main.java
@@ -9,9 +9,8 @@ import org.webbitserver.handler.StaticFileHandler;
 public class Main {
 
 	public static void main(String[] args) {
-//    	GreedGame theGame = new GreedGame(3);
-//    	theGame.startGame();
-	ArrayList<GreedConnection> connections = new ArrayList<GreedConnection>();
+    	
+    	ArrayList<GreedConnection> connections = new ArrayList<GreedConnection>();
 	    WebServer webServer = WebServers.createWebServer(8080);
 	    webServer.add(new StaticFileHandler("/static-files"));
 	    WebsocketHandler handler = new WebsocketHandler(connections);
