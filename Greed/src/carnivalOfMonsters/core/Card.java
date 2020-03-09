5c67be8d518ac707845a1ba1e68fe86e71832ddf
Watno <watno@arcor.de>
2020-03-09 12:08:58 +0100

    cleanup

--- Greed/src/carnivalOfMonsters/core/Card.java
+++ Greed/src/carnivalOfMonsters/core/Card.java
@@ -2,16 +2,16 @@ package carnivalOfMonsters.core;
 
 import com.google.gson.annotations.Expose;
 
-public abstract class Card implements ICard{
-	@Expose
-	private String name;
-	@Expose
-	private String type;
+public abstract class Card implements ICard {
+    @Expose
+    private String name;
+    @Expose
+    private String type;
 
-	protected Card(String type, String name) {
-		super();
-		this.name = name;
-		this.type = type;
-	}
-	
+    protected Card(String type, String name) {
+        super();
+        this.name = name;
+        this.type = type;
+    }
+
 }
